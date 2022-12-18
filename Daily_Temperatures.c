ea32cd07564019f79bf9ee60cbef3c40371a9b46
ThatCT <56414597+ThatCT@users.noreply.github.com>
2022-12-18 03:28:28 +0100

    Create Daily_Temperatures.c

---
+++ Daily_Temperatures.c
@@ -0,0 +1,23 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* dailyTemperatures(int* temperatures, int temperaturesSize, int* returnSize){
+    int j;
+    int* answer = (int*)calloc(temperaturesSize, sizeof(int));
+    int max_val = 0;
+    *returnSize = temperaturesSize;
+
+    for(int i = temperaturesSize-1; i >= 0; i--){
+        answer[i] = 0;
+        if (temperatures[i] >= max_val){
+            max_val =  temperatures[i]; 
+            continue;
+        }
+        j= 1;
+        while (temperatures[j+i]<=temperatures[i]) {
+            j += answer[j+i];
+        }
+        answer[i] = j;
+    }
+    return answer;
+}
